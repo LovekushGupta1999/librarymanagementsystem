31bc2bb32d673be4e4239eb1165339e6380b3fdb
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-01-26 01:05:35 +0530

    fetch function

--- script.js
+++ script.js
@@ -15,19 +15,52 @@ function signup(){
     }
     else{
     localStorage.setItem("signup", JSON.stringify(data))
+  
     }
 }
 
 
 let signup_data=JSON.parse(localStorage.getItem("signup"));
 
+// ................................signup apply...............................
+
+function addAdmin(){
+    let data=
+        {
+            email: document.getElementById("input1").value,
+            pass: document.getElementById("input2").value,
+            cpass: document.getElementById("input3").value
+        //    date: new Date().getDate()
+        }
+    
+    if(data.pass!=data.cpass){
+        seterror("alertpass","*password not match")
+        return false;
+    }
+    else{
+    localStorage.setItem("newadmin", JSON.stringify(data))
+    Swal.fire({
+        title: "Submitted",
+        text: "You clicked the button!",
+        icon: "success",
+        confirmButtonText: "ok"
+       
+      });
+   return true
+    }
+}
+
+
+let newadmin_data=JSON.parse(localStorage.getItem("newadmin"));
+
+
 
 // ...........................User login.apply...........................
 
 
 function userlogin(){
-    let loginEmail=document.getElementById("email").value;
-    let loginPass=document.getElementById("password").value;
+    let loginEmail=document.getElementById("input1").value;
+    let loginPass=document.getElementById("input2").value;
 
     if(signup_data.email!=loginEmail || signup_data.pass!=loginPass){
         Swal.fire({
@@ -40,28 +73,20 @@ function userlogin(){
     }
 }
 
-// ...........................admin login.apply...........................
 
 
-function adminlogin(){
-    let admin_data={
-        email: "lovekush@123",
-        pass: "12345"
-    }
 
-    let loginEmail=document.getElementById("email").value;
-    let loginPass=document.getElementById("password").value;
+ 
 
-    if(admin_data.email!=loginEmail || admin_data.pass!=loginPass){
-        // Swal.fire({
-        //     icon: "error",
-        //     title: "Oops...",
-        //     text: "Something went wrong!",
-        //     footer: '<a href="#">User Not Found?</a>'
-        //   });
-        return false;
-    }
-}
+
+
+// ....................................function for display active admin......................................
+
+// let adminkey=JSON.parse(localStorage.getItem("adminkey"));
+// function adminIddisplay(id){
+//     let adminId=document.querySelector("#adminId");
+//     adminId.innerText=`Admin : ${id}`;
+// }
 
 
 
@@ -82,7 +107,8 @@ function adminwindow(){
     <div id="btn"></div>
     <div class="continer">
         
-        <form action="admindashboard.html" onsubmit=" return adminlogin()">
+        <form  onsubmit=" return fetchAdmindata()
+">
             <div class="from-group">
                 <input type="email" id="input1" required>
               <label for="input1">Email :</label>
@@ -110,7 +136,7 @@ function adminwindow(){
              <p>
                 <input type="checkbox"> Remeber me <a href="#">Forgot Password</a>
             </p>
-              <input id="btn" type="submit" value="signup">
+              <input id="btn" type="submit" value="LOGIN">
 
                <p>Don't have a Account ? <a href="#">Register</a></p> 
         </form>
@@ -161,7 +187,7 @@ function Userwindow(){
              <p>
                 <input type="checkbox"> Remeber me <a href="#">Forgot Password</a>
             </p>
-              <input id="btn" type="submit" value="signup">
+              <input id="btn" type="submit" value="LOGIN">
 
                <p>Don't have a Account ? <a href="#">Register</a></p> 
         </form>
@@ -223,3 +249,120 @@ function Registerwindow(){
         confirmButtonText: 'Close'
     });
 };
+
+
+function addAdminWindow(){
+    Swal.fire({
+        title: 'REGISTER ADMIN',
+        html: `
+    <section id="signup_sec">
+    <div id="btn"></div>
+    <div class="continer">
+        
+        <form action="#" onsubmit=" return InsertAdmindata()">
+            <div class="from-group">
+                <input type="email" id="input1" required>
+              <label for="input1">Email :</label>
+              <i class="fa-regular fa-envelope"></i>
+            </div>
+            <p id="alertmes"></p>
+              
+
+              <div class="from-group">
+                <input type="password"  id="input2" required>
+                <label for="input2">password :</label>
+                <i class="fa-solid fa-lock"></i>
+
+              </div>
+            <p id="alertpass"></p>
+
+               <div class="from-group">
+                <input type="text"id="input3" required>
+                <label for="input3">Confirm password :</label>
+                <i class="fa-solid fa-lock"></i>
+
+              </div>
+            <p id="alertmes"></p> 
+
+          <!--   <p>
+                <input type="checkbox"> Remeber me <a href="#">Forgot Password</a>
+            </p> --!>
+            <p></p>
+              <input id="btn" type="submit" value="signup">
+
+              <!-- <p>Don't have a Account ? <a href="#">Register</a></p> -->
+        </form>
+    
+    </div>
+  </section>
+        `,
+        // icon: 'info',
+        confirmButtonText: 'Close'
+    });
+};
+//-------------------------------------------set data in database--------------------------------
+async function InsertAdmindata(){
+  let admindata=
+        {
+            email: document.getElementById("input1").value,
+            pass: document.getElementById("input2").value,
+            cpass: document.getElementById("input3").value
+        //    date: new Date().getDate()
+        }
+    
+    if(admindata.pass!=admindata.cpass){
+        seterror("alertpass","*password not match")
+        return false;
+    }
+    
+    
+  fetch("http://localhost:3000/admin",{
+    method: "POST",
+    headers:{'content-type':'admindata/json'},
+    body: JSON.stringify(admindata)
+
+  }).then(r=>Swal.fire({
+     title: "Submitted",
+     text: "You clicked the button!",
+     icon: "success",
+     confirmButtonText: "ok"
+    
+   }));
+   return true;
+}
+
+// ...........................admin login.apply...........................................
+
+
+async function fetchAdmindata(){
+  let res= await fetch ("http://localhost:3000/admin")
+ let data= await res.json();
+  data.map((admin)=>{
+
+
+    let loginEmail=document.getElementById("input1").value;
+    let loginPass=document.getElementById("input2").value;
+
+    if(admin.email==loginEmail && admin.pass==loginPass){
+      Swal.fire({
+        title: "Submitted",
+        text: "You clicked the button!",
+        icon: "success",
+        confirmButtonText: "ok"
+       
+      });
+      let adminId=document.querySelector("#adminId");
+      adminId.innerText= "Admin : "+admin.email; 
+      return true;
+    }
+  }
+)
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">User Not Found?</a>'
+          });
+        return false;
+    
+        }
